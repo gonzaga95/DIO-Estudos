a49416f9a96bd78559932f11ec3b272a5c82a0eb
Carlos Eduardo Gonzaga <91717487+gonzaga95@users.noreply.github.com>
2022-07-26 21:12:19 -0400

    finalizado incluindo instrucoes readme

---
+++ introducao-javascript/projeto-contador/README.md
@@ -0,0 +1,11 @@
+# Contador
+
+Neste projeto, iremos fazer a nossa primeira integração de um código Javascript com uma página HTML.
+
+## Desafios para ir além
+
+1. Tente implementar os eventos usando `addEventListener`.
+2. Crie condicionais para desabilitar o botão de incrementar ou decrementar quando o `count` chegar a um determinado valor.
+3. Mude a cor do texto em `currentNumber` para vermelho quando o valor for negativo e verde quando positivo.
+
+> Introdução ao JavaScript na DIO.me
--- introducao-javascript/projeto-contador/assets/js/scripts.js
+++ introducao-javascript/projeto-contador/assets/js/scripts.js
@@ -1,12 +1,58 @@
-let currentNumberWrapper = document.querySelector("#current-number");
+const currentNumberWrapper = document.querySelector("#current-number");
 let currentNumber = 0;
 
-function increment() {
-    currentNumber += 1;
-    currentNumberWrapper.innerHTML = currentNumber;
-}
+const decrementButton = document.querySelector("#decrement");
+const incrementButton = document.querySelector("#increment");
 
-function decrement() {
+decrementButton.addEventListener("click", function () {
     currentNumber -= 1;
     currentNumberWrapper.innerHTML = currentNumber;
-}
+    if (currentNumber < 0) {
+        currentNumberWrapper.style.color = "#9B0000";
+    } else if (currentNumber === 0) {
+        currentNumberWrapper.style.color = "black";
+    }
+    if (currentNumber <= -10) {
+        decrementButton.style.visibility = "hidden";
+    } else {
+        incrementButton.style.visibility = "visible";
+    }
+});
+
+incrementButton.addEventListener("click", function () {
+    currentNumber += 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+    if (currentNumber > 0) {
+        currentNumberWrapper.style.color = "#006400";
+    } else if (currentNumber === 0) {
+        currentNumberWrapper.style.color = "black";
+    }
+    if (currentNumber >= 10) {
+        incrementButton.style.visibility = "hidden";
+    } else {
+        decrementButton.style.visibility = "visible";
+    }
+});
+
+// currentNumberWrapper.addEventListener("change", function () {
+//     if (currentNumber > 0) {
+//         currentNumberWrapper.style.color = "#006400";
+//         if (currentNumber >= 10) {
+//             incrementButton.style.display = "none";
+//         } else {
+//             incrementButton.style.display = "block";
+//         }
+//     } else if (currentNumber === 0) {
+//         currentNumberWrapper.style.color = "black";
+//     }
+//     if (currentNumber < 0) {
+//         currentNumberWrapper.style.color = "#9B0000";
+//         if (currentNumber <= -10) {
+//             decrementButton.style.visibility = "hidden";
+//         } else {
+//             decrementButton.style.display = "block";
+//         }
+//     } else if (currentNumber === 0) {
+//         currentNumberWrapper.style.color = "black";
+//     }
+// });
