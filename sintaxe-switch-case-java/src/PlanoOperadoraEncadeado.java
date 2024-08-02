7094f2e800dc6be8ff3a14d254c27722b5deed32
gonzaga95 <gonzagas.dudu@gmail.com>
2024-08-02 14:41:45 -0400

    feat plano operadora: add readme

--- sintaxe-switch-case-java/src/PlanoOperadoraEncadeado.java
+++ sintaxe-switch-case-java/src/PlanoOperadoraEncadeado.java
@@ -4,6 +4,15 @@ public class PlanoOperadoraEncadeado {
 
         if (plano.equals("B")) {
             System.out.println("100 minutos de ligações para qualquer operadora");
+
+            }
+        else if (plano.equals("M")) {
+            System.out.println("WhatsApp e Instagram com dados ilimitados");
+            System.out.println("100 minutos de ligações para qualquer operadora");
+        } else if (plano.equals("T")) {
+            System.out.println("5 GB de dados para YouTube");
+            System.out.println("WhatsApp e Instagram com dados ilimitados");
+            System.out.println("100 minutos de ligações para qualquer operadora");
         }
     }
 }
