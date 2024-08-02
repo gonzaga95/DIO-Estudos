dc9158c82fe069d4106b108162eac9baaa7fb0e6
gonzaga95 <gonzagas.dudu@gmail.com>
2024-08-02 14:34:13 -0400

    feat plano operadora: treinando sintaxe de if-else

---
+++ sintaxe-switch-case-java/src/PlanoOperadoraEncadeado.java
@@ -0,0 +1,9 @@
+public class PlanoOperadoraEncadeado {
+    public static void main(String[] args) {
+        String plano = "T"; // (T) turbo, (M) mídia, (B) básico.
+
+        if (plano.equals("B")) {
+            System.out.println("100 minutos de ligações para qualquer operadora");
+        }
+    }
+}
